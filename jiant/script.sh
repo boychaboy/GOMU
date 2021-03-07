7ebade8ea8712a9e9dd133df862f5c22ebcee786
boychaboy <hoon2j@gmail.com>
2021-03-08 01:38:50 +0900

    feat: script update for glue

--- jiant/script.sh
+++ jiant/script.sh
@@ -2,18 +2,18 @@ EXP_DIR=../
 
 GPU_ID=$1
 
-CUDA_VISIBLE_DEVICES=$GPU_ID python -m jiant.scripts.download_data.runscript \
-    download \
-    --benchmark GLUE \
-    --output_path ${EXP_DIR}/GLUE
-#CUDA_VISIBLE_DEVICES=$GPU_ID python -m jiant.proj.simple.runscript \
-    #run \
-    #--run_name simple \
-    #--exp_dir ${EXP_DIR}/ \
-    #--data_dir ${EXP_DIR}/tasks \
-    #--hf_pretrained_model_name_or_path roberta-base \
-    #--tasks mrpc \
-    #--train_batch_size 128 \
-    #--num_train_epochs 5 \
-    #--write_test_preds \
-    #--do_save_best
+#CUDA_VISIBLE_DEVICES=$GPU_ID python -m jiant.scripts.download_data.runscript \
+    #download \
+    #--benchmark GLUE \
+    #--output_path ${EXP_DIR}/GLUE
+CUDA_VISIBLE_DEVICES=$GPU_ID python -m jiant.proj.simple.runscript \
+    run \
+    --run_name glue \
+    --exp_dir ${EXP_DIR}/ \
+    --data_dir ${EXP_DIR}/GLUE \
+    --hf_pretrained_model_name_or_path bert-base-uncased \
+    --tasks cola sst mrpc qqp stsb mnli mnli_mismatched qnli rte wnli glue_diagnostics \
+    --train_batch_size 128 \
+    --num_train_epochs 10 \
+    --write_test_preds \
+    --do_save_best
