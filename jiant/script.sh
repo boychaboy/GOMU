2be0e49fe35c5f6f0fee58e95eeac1896a029648
boychaboy <hoon2j@gmail.com>
2021-03-08 01:47:23 +0900

    feat: script update for glue

--- jiant/script.sh
+++ jiant/script.sh
@@ -6,13 +6,14 @@ GPU_ID=$1
     #download \
     #--benchmark GLUE \
     #--output_path ${EXP_DIR}/GLUE
+
 CUDA_VISIBLE_DEVICES=$GPU_ID python -m jiant.proj.simple.runscript \
     run \
     --run_name glue \
     --exp_dir ${EXP_DIR}/ \
     --data_dir ${EXP_DIR}/GLUE \
     --hf_pretrained_model_name_or_path bert-base-uncased \
-    --tasks cola sst mrpc qqp stsb mnli mnli_mismatched qnli rte wnli glue_diagnostics \
+    --tasks cola,sst \
     --train_batch_size 128 \
     --num_train_epochs 10 \
     --write_test_preds \
